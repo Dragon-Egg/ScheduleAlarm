7ee53ac8b9628445b35d62ba066506a3bb2e6c1b
Crow314 <crow31415@gmail.com>
2017-10-23 23:37:00 +0900

    MainActivityを.activityパッケージに移動

--- app/src/main/java/org/dragon_egg/schedulealarm/activity/MainActivity.java
+++ app/src/main/java/org/dragon_egg/schedulealarm/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package org.dragon_egg.schedulealarm;
+package org.dragon_egg.schedulealarm.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.dragon_egg.schedulealarm.R;
 
 public class MainActivity extends AppCompatActivity {
 
